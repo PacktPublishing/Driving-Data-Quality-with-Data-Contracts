85de21bd7ee22ed9227fb68b631b017e4a0ee075
Andrew Jones <andrew@andrew-jones.com>
2023-05-01 11:35:19 +0100

    Add pulumi_introduction example code

---
+++ Chapter08/pulumi_introduction/__main__.py
@@ -0,0 +1,32 @@
+"""A Google Cloud Python Pulumi program"""
+
+
+import pulumi
+from pulumi_gcp import bigquery
+
+default_dataset = bigquery.Dataset(
+    "defaultDataset",
+    dataset_id="pulumi_introduction",
+    friendly_name="Pulumi Introduction",
+    description="This is an example description",
+    location="US",
+)
+default_table = bigquery.Table(
+    "defaultTable",
+    dataset_id=default_dataset.dataset_id,
+    table_id="my_table",
+    deletion_protection=False,
+    schema="""[
+  {
+    "name": "id",
+    "type": "STRING",
+    "mode": "REQUIRED",
+    "description": "The ID"
+  },
+  {
+    "name": "state",
+    "type": "STRING",
+    "description": "State where the head office is located"
+  }
+]
+""")
