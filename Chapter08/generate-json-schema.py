8be030902a94093e631d2e0e087f109ca25ef9df
Andrew Jones <andrew@andrew-jones.com>
2023-05-02 11:15:55 +0100

    Add a script to validate data using JSON Schema

--- Chapter08/generate-json-schema.py
+++ Chapter08/generate-json-schema.py
@@ -3,11 +3,12 @@
 import sys
 sys.path.append("lib/")
 
+import json
 from data_contracts import DataContract
 
 data_contract = DataContract("contracts/Customer.yaml")
 
 output_file = 'customer.schema.json'
 with open(output_file, 'w') as writer:
-    writer.write(data_contract.jsonSchema())
+    writer.write(json.dumps(data_contract.jsonSchema(), indent=2))
     print(f'Written JSON Schema of `{data_contract.name()}` data contract to `{output_file}`')
---
+++ Chapter08/validate-data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.append("lib/")
+
+from data_contracts import DataContract
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
+events = [
+    # Valid events
+    {"id": "DC12", "name" : "Andrew", "email" : "andrew@data-contracts.com", "language": "en"},
+    {"id": "DC13", "name" : "Deborah", "email" : "deborah@data-contracts.com"},
+    # Missing email, which is a required field
+    {"id": "DC14", "name" : "Bukayo", "language": "en"},
+    # Email does not pass regex validation
+    {"id": "DC15", "name" : "Bukayo", "email" : "bukayo", "language": "en"},
+    # `nl` is not a valid language code
+    {"id": "DC16", "name" : "Vivianne", "email" : "vivianne@data-contracts.com", "language": "nl"},
+]
+
+data_contract = DataContract("contracts/Customer.yaml")
+
+for event in events:
+    try:
+        validate(event, data_contract.jsonSchema())
+        print(f"✅ Successfully validated event {event}")
+    except ValidationError as e:
+        print(f"❗ Error validating event {event}\n{e}")
